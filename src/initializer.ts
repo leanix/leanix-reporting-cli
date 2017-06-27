2e38b0c3cf78b8c22ac764c538e0d9973b527a98
Patrick Surrey <patrick.surrey@leanix.net>
2017-06-27 13:46:28 +0200

    [#146839397] Further work on initializer

--- src/initializer.ts
+++ src/initializer.ts
@@ -1,28 +1,94 @@
 import * as chalk from 'chalk';
-import * as vr from 'variable-replacer';
+import * as varReplace from 'variable-replacer';
 import * as path from 'path';
 import * as process from 'process';
-import { PathHelper } from "./path-helper";
+import { PathHelper } from './path-helper';
+import * as inquirer from 'inquirer';
+import { spawn } from 'child_process';
 
 export class Initializer {
   private pathHelper = new PathHelper();
 
-  public init() {
+  public init(): Promise<any> {
     console.log(chalk.green('Initializing new project...'));
-    this.extractTemplateFile('package.json');
+
+    return inquirer.prompt(this.getInquirerQuestions())
+    .then(answers => {
+      this.extractTemplateFiles(answers);
+      return this.installViaNpm();
+    });
+
   }
 
-  private extractTemplateFile(filename: string) {
+  private extractTemplateFiles(answers: any) {
+    console.log(chalk.green('Extracting template files...'));
+    this.extractTemplateFile('package.json', answers);
+    this.extractTemplateFile('index.html', answers);
+  }
+
+  private extractTemplateFile(filename: string, answers: any) {
     const source = this.pathHelper.getTemplateSourcePath(filename);
     const dest = this.pathHelper.getTargetFolderPath(filename);
+
     console.log(source, dest);
-    vr({
+
+    varReplace({
       source,
       dest,
-      inlineData: {
-        name: 'A_NAME'
-      },
+      inlineData: answers,
       logLevel: 'info' // none
+    });
+  }
+
+  private getInquirerQuestions(): inquirer.Questions {
+    // The name properties correspond to the variables in the package.json template file
+    return [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Name of your project for package.json'
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: 'Unique id for this report in Java package notation (e.g. net.leanix.barcharts)'
+      },
+      {
+        type: 'input',
+        name: 'author',
+        message: 'Who is the author of this report (e.g. LeanIX GmbH)'
+      },
+      {
+        type: 'input',
+        name: 'title',
+        message: 'A title to be shown in LeanIX when report is installed'
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Description of your project'
+      }
+    ];
+  }
+
+  private installViaNpm(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      console.log(chalk.green('Installing project dependencies via npm...'));
+      const installProc = spawn('npm', ['install']);
+/*
+      installProc.stdout.on('data', (data) => {
+        console.log(chalk.yellow(data.toString()));
+      });
+*/
+      installProc.on('close', (exitCode) => {
+        if (exitCode === 0) {
+          console.log(chalk.green('npm install successful!'));
+          resolve();
+        } else {
+          console.log(chalk.red('npm install failed!'));
+          reject();
+        }
+      });
     });
   }
 }
