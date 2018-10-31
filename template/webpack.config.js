3363674509292dbf98c9b280356d90bb39a52824
Guido Lorenz <guido.lorenz@leanix.net>
2018-10-31 13:47:57 +0100

    TNDO-121 Update dependencies

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env']
+            presets: ['@babel/preset-env']
           }
         }
       },
