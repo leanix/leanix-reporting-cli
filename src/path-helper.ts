955bde89aeee92320d18d6ed96bd5af947919c8c
Guido Lorenz <guido.lorenz@leanix.net>
2020-11-18 13:56:47 +0100

    TNDO-2645 Remove unused functions

--- src/path-helper.ts
+++ src/path-helper.ts
@@ -2,10 +2,6 @@ import * as path from 'path';
 
 export class PathHelper {
 
-  public getSourceDirectory(): string {
-    return path.resolve(__dirname, '..');
-  }
-
   public getProjectDirectory(): string {
     return process.cwd();
   }
@@ -16,15 +12,5 @@ export class PathHelper {
 
   public getTemplateDirectory(): string {
     return path.resolve(__dirname, '../template');
-  }
-
-  public getTemplateSourcePath(templateFileName: string): string {
-    const sourceDir = this.getSourceDirectory();
-    return path.resolve(sourceDir, 'template', templateFileName);
-  }
-
-  public getTargetFolderPath(filename: string): string {
-    const targetDir = this.getProjectDirectory();
-    return path.resolve(targetDir, filename);
   }
 }
