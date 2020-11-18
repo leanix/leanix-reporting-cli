48e8ef4455c5c18214db4e7ec91ce5ee11b19f56
Guido Lorenz <guido.lorenz@leanix.net>
2020-11-18 17:27:32 +0100

    TNDO-2645 Make dist path and build command configurable

--- src/builder.ts
+++ src/builder.ts
@@ -8,9 +8,6 @@ import { promisify } from 'util';
 const execAsync = promisify(exec);
 const rimrafAsync = promisify(rimraf);
 
-/**
- * Builds the project using webpack into 'dist' folder.
- */
 export class Builder {
 
   private projectDir = new PathHelper().getProjectDirectory();
@@ -18,9 +15,11 @@ export class Builder {
   public async build(): Promise<void> {
     console.log(chalk.yellow(chalk.italic('Building...')));
 
+    const buildConfig = this.getBuildConfig();
+
     try {
-      await this.removeDistDir();
-      const { stdout } = await this.buildWithWebpack();
+      await this.removeDistDir(buildConfig.distPath);
+      const { stdout } = await this.doBuild(buildConfig.buildCommand);
 
       console.log(stdout);
       console.log(chalk.green('\u2713 Project successfully build!'));
@@ -29,13 +28,23 @@ export class Builder {
     }
   }
 
-  private removeDistDir() {
-    const distDir = path.resolve(this.projectDir, 'dist');
+  private getBuildConfig() {
+    const packageJson = require(path.resolve(this.projectDir, 'package.json')); // eslint-disable-line @typescript-eslint/no-var-requires
+    const leanixReportingCli = packageJson.leanixReportingCli || {};
+
+    return {
+      distPath: leanixReportingCli.distPath ?? './dist',
+      buildCommand: leanixReportingCli.buildCommand ?? 'node_modules/.bin/webpack'
+    }
+  }
+
+  private removeDistDir(distPath: string) {
+    const distDir = path.resolve(this.projectDir, distPath);
     return rimrafAsync(distDir);
   }
 
-  private buildWithWebpack() {
-    const webpackCmd = path.resolve(this.projectDir, 'node_modules/.bin/webpack');
+  private doBuild(buildCommand: string) {
+    const webpackCmd = path.resolve(this.projectDir, buildCommand);
     return execAsync(webpackCmd);
   }
 }
