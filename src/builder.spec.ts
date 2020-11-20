a4bbe15b351ca292dbe1764cd43281eec24a88bd
Guido Lorenz <guido.lorenz@leanix.net>
2020-11-20 14:54:20 +0100

    TNDO-2645 Test custom dist paths

--- src/builder.spec.ts
+++ src/builder.spec.ts
@@ -1,5 +1,46 @@
+import { Builder } from './builder';
+
 describe('Builder', () => {
-  it('works', () => {
-    expect(true).toBe(true);
+  const processCwd = jest.spyOn(process, 'cwd').mockReturnValue('/home/johndoe/my-report');
+
+  const loadPackageJson = jest.fn();
+  const rimraf = jest.fn().mockResolvedValue(undefined);
+  const exec = jest.fn().mockResolvedValue({ stdout: 'stdout', stderr: 'stderr' });
+
+  const builder = new Builder(loadPackageJson, rimraf, exec);
+
+  afterEach(() => {
+    loadPackageJson.mockClear();
+    rimraf.mockClear();
+    exec.mockClear();
+  });
+
+  afterAll(() => {
+    processCwd.mockRestore();
+  });
+
+  it('builds with default configuration', async () => {
+    loadPackageJson.mockReturnValueOnce({});
+
+    await builder.build();
+
+    expect(rimraf).toHaveBeenCalledWith('/home/johndoe/my-report/dist');
+    expect(exec).toHaveBeenCalledWith('/home/johndoe/my-report/node_modules/.bin/webpack');
+  });
+
+  it.each([
+    ['public', '/home/johndoe/my-report/public'],
+    ['./public', '/home/johndoe/my-report/public'],
+    ['/tmp/dist', '/tmp/dist']
+  ])('removes custom dist path "%s"', async (distPath, expectedPath) => {
+    loadPackageJson.mockReturnValueOnce({
+      leanixReportingCli: {
+        distPath
+      }
+    });
+
+    await builder.build();
+
+    expect(rimraf).toHaveBeenCalledWith(expectedPath);
   });
 });
