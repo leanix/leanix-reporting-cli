f811ae1d57d42c95b03286f5e34b3b8296478d40
Guido Lorenz <guido.lorenz@leanix.net>
2020-11-20 13:56:53 +0100

    TNDO-2645 Refactor builder to use async

--- src/builder.ts
+++ src/builder.ts
@@ -3,6 +3,10 @@ import { exec } from 'child_process';
 import { PathHelper } from './path-helper';
 import * as path from 'path';
 import * as rimraf from 'rimraf';
+import { promisify } from 'util';
+
+const execAsync = promisify(exec);
+const rimrafAsync = promisify(rimraf);
 
 /**
  * Builds the project using webpack into 'dist' folder.
@@ -11,28 +15,27 @@ export class Builder {
 
   private projectDir = new PathHelper().getProjectDirectory();
 
-  public build(): Promise<void> {
+  public async build(): Promise<void> {
     console.log(chalk.yellow(chalk.italic('Building...')));
-    return this.buildWithWebpack();
+
+    try {
+      await this.removeDistDir();
+      const { stdout } = await this.buildWithWebpack();
+
+      console.log(stdout);
+      console.log(chalk.green('\u2713 Project successfully build!'));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  private removeDistDir() {
+    const distDir = path.resolve(this.projectDir, 'dist');
+    return rimrafAsync(distDir);
   }
 
   private buildWithWebpack() {
-    return new Promise<void>((resolve, reject) => {
-      // remove dist folder
-      rimraf(path.resolve(this.projectDir, 'dist'), () => {
-        const webpackCmd = path.resolve(this.projectDir, 'node_modules/.bin/webpack');
-        exec(webpackCmd, (err, stdout) => {
-          if (err) {
-            console.error(err);
-            reject(err);
-          } else {
-            console.log(stdout);
-            console.log(chalk.green('\u2713 Project successfully build!'));
-            resolve();
-          }
-        });
-      })
-    });
+    const webpackCmd = path.resolve(this.projectDir, 'node_modules/.bin/webpack');
+    return execAsync(webpackCmd);
   }
-
 }
