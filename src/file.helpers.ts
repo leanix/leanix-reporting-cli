c28739744f640fe627d970809f51f38fba1b134c
Guido Lorenz <guido.lorenz@leanix.net>
2020-11-23 15:54:40 +0100

    TNDO-2645 Use generic `readJsonFile()` to reduce explicit type casting

--- src/file.helpers.ts
+++ src/file.helpers.ts
@@ -3,17 +3,17 @@ import { readFileSync } from 'fs';
 import { getProjectDirectoryPath } from './path.helpers';
 import { LxrConfig, PackageJson } from './interfaces';
 
-export function readJsonFile(path: string): unknown {
+export function readJsonFile<T>(path: string): T {
   const buffer = readFileSync(path);
   return JSON.parse(buffer.toString('utf-8'));
 }
 
 export function loadLxrConfig(): LxrConfig {
   const lxrConfigPath = getProjectDirectoryPath('lxr.json')
-  return readJsonFile(lxrConfigPath) as LxrConfig;
+  return readJsonFile(lxrConfigPath);
 }
 
 export function loadPackageJson(): PackageJson {
   const packageJsonPath = getProjectDirectoryPath('package.json')
-  return readJsonFile(packageJsonPath) as PackageJson;
+  return readJsonFile(packageJsonPath);
 }
