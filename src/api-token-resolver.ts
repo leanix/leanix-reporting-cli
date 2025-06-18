bc84266eb334d38d808007df0209fe93e0e58a0c
Paulo Santos <paulo.santos04@sap.com>
2025-06-18 16:08:55 -0300

    ATL-308 Update ApiTokenResolver proxy setup

--- src/api-token-resolver.ts
+++ src/api-token-resolver.ts
@@ -1,16 +1,20 @@
-import axios from 'axios';
+import axios, { type AxiosRequestConfig } from 'axios'
+import { getAxiosProxyConfiguration } from './helpers'
 
 export class ApiTokenResolver {
-  public static getAccessToken(host: string, apiToken: string, proxy?: string): Promise<string> {
-    const [proxyHost, proxyPort] = proxy?.split(':') ?? [];
-    const config = {
+  public static async getAccessToken(host: string, apiToken: string, proxy?: string): Promise<string> {
+    const config: AxiosRequestConfig = {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
-        Authorization: `Basic ${Buffer.from('apitoken:' + apiToken).toString('base64')}`
-      },
-      proxy: proxyHost && proxyPort ? { host: proxyHost, port: parseInt(proxyPort) } : undefined
-    };
-    const url = host + '/services/mtm/v1/oauth2/token?grant_type=client_credentials';
-    return axios.post(url, {}, config).then((response) => response.data['access_token']);
+        'Authorization': `Basic ${Buffer.from(`apitoken:${apiToken}`).toString('base64')}`
+      }
+    }
+    if (proxy) {
+      config.httpsAgent = getAxiosProxyConfiguration(proxy)
+    }
+    const url = `${host}/services/mtm/v1/oauth2/token?grant_type=client_credentials`
+    const accessToken = await axios.post(url, {}, config)
+      .then(response => response.data.access_token)
+    return accessToken
   }
 }
