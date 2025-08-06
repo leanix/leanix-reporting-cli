0ebe6d7ecc84277170747d0634649125f2af02fb
Paulo dos Santos <paulo.santos@leanix.net>
2025-08-06 18:45:24 -0300

    ATL-500 Revert changes to the CI workflow (#117)
    * ATL-500 Update form-data dependency and bump version
    
    * ATL-500 update ci workflow
    
    * ATL-500 Update codeowners
    
    * ATL-500 Update CI workflow
    
    * ATL-500 Update package-lock.json
    
    * ATL-500 Update package-lock.json
    
    * ATL-500 Update import style
    
    ---------
    
    Co-authored-by: Paulo Santos <paulo.santos04@sap.com>

--- src/api-token-resolver.ts
+++ src/api-token-resolver.ts
@@ -1,4 +1,5 @@
-import axios, { type AxiosRequestConfig } from 'axios'
+import type { AxiosRequestConfig } from 'axios'
+import axios from 'axios'
 import { getAxiosProxyConfiguration } from './helpers'
 
 export class ApiTokenResolver {
